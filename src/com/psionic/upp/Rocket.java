7024720325d34619b4c9dfc08a07020fca6f0acc
patryk-budzynski <pbudzynski@ganymede.eu>
2012-11-29 18:30:20 +0100

    czystka

--- src/com/psionic/upp/Rocket.java
+++ src/com/psionic/upp/Rocket.java
@@ -1,17 +1,12 @@
 package com.psionic.upp;
 
+import com.PsichiX.XenonCoreDroid.XeAssets;
 import com.PsichiX.XenonCoreDroid.HighLevel.Graphics.Image;
 import com.PsichiX.XenonCoreDroid.HighLevel.Graphics.Material;
 
-public class Rocket extends Actor {
-
-	public Rocket(Material mat) {
-		super(mat);
+public class Rocket extends Actor
+{
+	public Rocket(XeAssets assets, int sheetResId, String subImage) {
+		super(assets, sheetResId, subImage);
 	}
-	
-	public Rocket(Material mat,Image img) {
-		super(mat,img);
-	}
-	
-	
 }
