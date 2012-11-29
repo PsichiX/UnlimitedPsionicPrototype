71309e29eda8b2410d9be15c866337cf50160bda
kubusabadi <jakubhejwowski@MacBook-Pro-Jakub.local>
2012-11-29 12:51:26 +0100

    background

---
+++ src/com/psionic/upp/Background.java
@@ -0,0 +1,34 @@
+package com.psionic.upp;
+
+import com.PsichiX.XenonCoreDroid.XeAssets;
+import com.PsichiX.XenonCoreDroid.HighLevel.Graphics.Camera2D;
+import com.PsichiX.XenonCoreDroid.HighLevel.Graphics.Image;
+import com.PsichiX.XenonCoreDroid.HighLevel.Graphics.Material;
+import com.PsichiX.XenonCoreDroid.HighLevel.Graphics.Sprite;
+
+public class Background extends Sprite {
+
+	public Background(Material mat) {
+		super(mat);
+	}
+	
+	float size;
+	
+	public Background(XeAssets assets, Camera2D cam){
+		super(null);
+		Material bgMat = (Material) assets.get(R.raw.material,Material.class);
+		Image bgImage = (Image) assets.get(R.drawable.background,Image.class);
+		size = (float)bgImage.getTexture().getWidth();
+		setMaterial(bgMat);
+		setSize(cam.getViewWidth(), cam.getViewHeight());
+		setTextureScaleFromImageAspect(bgImage, true);
+	}
+	
+	public void setOnDistance(float distance) {
+		
+		float d = (size != 0.0f) ? distance/size : 0.0f;
+		
+		setTextureOffset(d, 0.0f);
+	}
+
+}
--- src/com/psionic/upp/GameState.java
+++ src/com/psionic/upp/GameState.java
@@ -4,19 +4,33 @@ package com.psionic.upp;
 import com.PsichiX.XenonCoreDroid.XeApplication.State;
 import com.PsichiX.XenonCoreDroid.XeApplication.Touches;
 import com.PsichiX.XenonCoreDroid.HighLevel.Graphics.Camera2D;
+import com.PsichiX.XenonCoreDroid.HighLevel.Graphics.Image;
+import com.PsichiX.XenonCoreDroid.HighLevel.Graphics.Material;
 import com.PsichiX.XenonCoreDroid.HighLevel.Graphics.Scene;
+import com.PsichiX.XenonCoreDroid.HighLevel.Graphics.Sprite;
 
 public class GameState extends State
 {
 	Camera2D cam;
 	Scene scn;
-
+	
+	Background background;
+	
+	float distance;
+	float speed = 100.0f;
+	
 	@Override
 	public void onEnter()
 	{
 		scn = (Scene)getApplication().getAssets().get(R.raw.scene, Scene.class);
 		cam = (Camera2D)scn.getCamera();
 		cam.setViewPosition(cam.getViewWidth() * 0.5f, cam.getViewHeight() * 0.5f);
+		
+		background = new Background(getApplication().getAssets(),cam);
+		
+		distance = 0.0f;
+		
+		scn.attach(background);
 	}
 
 	@Override
@@ -30,7 +44,9 @@ public class GameState extends State
 	{
 		//float dt = 1.0f / 30.0f;
 		float dt = getApplication().getTimer().getDeltaTime() * 0.001f;
-
+		distance += dt * speed;
+		background.setOnDistance(distance);
+		
 		scn.update(dt);
 	}
 
